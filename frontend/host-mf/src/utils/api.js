444f0dcfca7f23cd69f2256c8a4504fc370a8ace
Anatoly Zimakov <yatolix@yandex.ru>
2025-04-06 01:48:17 +0300

    written clarification for selected approach and removed unnecessary styles in host which were duplicated in mf's

--- frontend/host-mf/src/utils/api.js
+++ frontend/host-mf/src/utils/api.js
@@ -38,20 +38,6 @@ class Api {
       .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
   }
 
-  setUserAvatar({ avatar }) {
-    return fetch(`${this._address}/${this._groupId}/users/me/avatar`, {
-      method: 'PATCH',
-      headers: {
-        authorization: this._token,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        avatar,
-      }),
-    })
-      .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
-  }
-
 }
 
 const api = new Api({
